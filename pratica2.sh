64298c1a09b6a621671d4a48689fc8864dfb2a74
Nelson Neto <nhsneto@gmail.com>
2026-07-07 11:18:31 -0300

    feat: cria pratica2.sh

---
+++ pratica2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo -e "Faltando nome do diretorio.\nFaltando nome do usuario.\nFaltando email do usuario."
+    exit 0;
+fi
+
+nome_dir=$1
+nome_usr=$2
+email_usr=$3
+
+mkdir $nome_dir
+cd $nome_dir
+echo "<!DOCTYPE html><html lang=\"pt-br\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device->
+" > index.html
+git init .
+git config user.name $nome_usr
+git config user.email $email_usr
+git add .
+git commit -m 'feat: cria index.html'
